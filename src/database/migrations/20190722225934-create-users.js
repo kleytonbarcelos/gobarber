23ee595a5bd42e61f1af1be6c21b6615b702bb79
Kleyton Barcelos <kleytonbarcelos@gmail.com>
2019-07-30 19:23:09 -0400

    rewrite migrations files

--- src/database/migrations/20190722225934-create-users.js
+++ src/database/migrations/20190722225934-create-users.js
@@ -1,12 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
       name: {
         type: Sequelize.STRING,
         allowNull: false,
